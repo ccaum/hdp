4684c56e48a9d92202d3b634a92c4386a6b3d1a9
Andrew C Wrenn <awrenn53@gmail.com>
2021-06-22 08:02:37 -0700

    docker-compose for HDP rather than LiDAR

--- manifests/app_stack.pp
+++ manifests/app_stack.pp
@@ -7,17 +7,49 @@
 # @param [Boolean] manage_docker
 #   Install and manage docker as part of app_stack
 #
-# @param [Integer] https_port
-#   Secure port number to access the HDP UI
+# @param [Integer] hdp_port
+#   Port to access HDP service
 #
+# @param [String[1]] hdp_user
+#   User to run HDP + all infra services as. Also owns mounted volumes
+#   
 # @param [String[1]] compose_version
 #   The version of docker-compose to install
 #
+# @param [String[1]] image_repository
+#   Image repository to pull images from - defaults to dockerhub.
+#   Can be used for airgapped environments/testing environments
+#
 # @param [String[1]] image_prefix
-#   The string that comes before the name of each
-#   container. This can be changed to support private
-#   image repositories such as for internal testing or
-#   air gapped environments.
+#   Prefix that comes before each image
+#   Can be used for easy name spacing under the same repository
+#
+# @param [String[1]] ca_server
+#   URL of Puppet CA Server. If no keys/certs are provided, then 
+#   HDP will attempt to provision its own certs and get them signed.
+#   Either this or ca_cert_file/key_file/cert_file can be specified.
+#   If autosign is not enabled, HDP will wait for the certificate to be signed
+#   by a puppet administrator
+#
+# @param [String[1]] ca_cert_file
+#   CA certificate to validate connecting clients
+#   This or ca_server can be specified
+#
+# @param [String[1]] key_file
+#   Private key for cert_file - pem encoded.
+#   This or ca_server can be specified
+#
+# @param [String[1]] cert_file
+#   Puppet PKI cert file - pem encoded.
+#   This or ca_server can be specified
+#
+# @param [String[1]] dns_name
+#   Name that puppet server will find HDP at.
+#   Should match the names in cert_file if provided.
+#   If ca_server is used instead, this name will be used as certname.
+#
+# @param [String[1]] hdp_version
+#   The version of the HDP containers to use
 #
 # @param [String[1]] hdp_version
 #   The version of the HDP containers to use
@@ -27,6 +59,10 @@
 #
 # @param [Optional[Array[String[1]]]] docker_users
 #   Users to be added to the docker group on the system
+#
+# @param [String[1]] max_es_memory
+#   Max memory for ES to use - in JVM -Xmx{$max_es_memory} format.
+#   Example: 4G, 1024M. Defaults to 4G.
 #
 # @example Use defalts or configure via Hiera
 #   include hdp::app_stack
@@ -42,11 +78,24 @@
 class hdp::app_stack (
   Boolean $create_docker_group = true,
   Boolean $manage_docker = true,
-  Integer $https_port = 443,
+  Integer $hdp_port = 9091,
+  String[1] $hdp_user = '11223',
   String[1] $compose_version = '1.25.0',
+  String[1] $image_repository = undef,
+
+  ## Either one of these two options can be configured
+  String[1] $ca_server = undef,
+
+  String[1] $ca_cert_file = undef,
+  String[1] $key_file = undef,
+  String[1] $cert_file = undef,
+
+
+  String[1] $dns_name = "hdp.puppet",
   String[1] $image_prefix = 'puppet/hdp-',
-  String[1] $hdp_version = '1.0.0-alpha.2',
+  String[1] $hdp_version = '0.0.1',
   String[1] $log_driver = 'journald',
+  String[1] $max_es_memory = '4G',
   Optional[Array[String[1]]] $docker_users = undef,
 ){
   if $create_docker_group {
@@ -77,14 +126,28 @@ class hdp::app_stack (
     '/opt/puppetlabs/hdp':
       ensure => directory,
       mode   => '0775',
+      owner   => $hdp_user,
+      group   => $hdp_user,
+    ;
+    '/opt/puppetlabs/hdp/ssl':
+      ensure => directory,
+      mode   => '0700',
+      owner   => $hdp_user,
+      group   => $hdp_user,
     ;
     '/opt/puppetlabs/hdp/docker-compose.yaml':
       ensure  => file,
       mode    => '0440',
       content => epp('hdp/docker-compose.yaml.epp', {
-        'image_prefix'  => $image_prefix,
-        'hdp_version' => $hdp_version,
-        'https_port'    => $https_port,
+        'hdp_version'      => $hdp_version,
+        'image_prefix'     => $image_prefix,
+        'image_repository' => $image_repository,
+        'hdp_port'         => $hdp_port,
+        'ca_server'        => $ca_server,
+        'dns_name'         => $dns_name,
+        'hdp_user'         => $hdp_user,
+        'root_dir'          => '/opt/puppetlabs/hdp',
+        'max_es_memory'    => $max_es_memory,
       }),
     ;
   }
